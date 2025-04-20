b2e461f54cdae2acbe6c1d4c8073f1020ce1e9af
Keiran <keiran0@proton.me>
2025-04-20 18:13:56 +0100

    im testing on prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { HeaderCardProps } from "~/lib/data/home";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-12">
-      <div className="max-w-4xl mx-auto">
+    <main className="container mx-auto px-4 py-12 flex items-center justify-center min-h-screen">
+      <div className="max-w-4xl w-full">
         <HeaderCard
           {...HeaderCardProps[0]}
         />
