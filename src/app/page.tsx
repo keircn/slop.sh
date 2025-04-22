c82c6328eaa0791e99b5a6623177085c0d7008c8
Keiran <keiran0@proton.me>
2025-04-22 05:25:54 +0100

    Disable projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { HeaderCard } from "../components/HeaderCard";
 import { HeaderCardProps } from "~/lib/data/home";
 import { PageTransition } from "~/components/PageTransition";
-import { ProjectsSection } from "~/components/ProjectsSection";
+// import { ProjectsSection } from "~/components/ProjectsSection";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="container mx-auto flex flex-col items-center justify-center mt-16 pb-16">
         <div className="max-w-6xl w-full">
           <HeaderCard {...HeaderCardProps[0]} />
-          <ProjectsSection />
+          {/* <ProjectsSection /> */}
         </div>
       </main>
     </PageTransition>
