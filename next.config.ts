29f7fb9b811a1d49b801a06ce2b0a785c6ef18d5
Keiran <keiran0@proton.me>
2025-04-30 23:29:22 +0100

    fix: use wildcard to allowed domains for discord presence

--- next.config.ts
+++ next.config.ts
@@ -5,27 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.discordapp.com",
-        port: "",
+        hostname: "*",
         pathname: "/**",
       },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/**",
-      }, {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "slop.sh",
-        port: "",
-        pathname: "/**",
-      }
     ],
   },
   poweredByHeader: false,
