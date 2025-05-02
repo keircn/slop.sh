0247f9460c892c5980551a923ebb0c839a8f51ab
Keiran <keiran0@proton.me>
2025-05-02 23:55:26 +0100

    style: formatting

--- src/components/NavbarLogo.tsx
+++ src/components/NavbarLogo.tsx
@@ -17,7 +17,11 @@ export function NavbarLogo({ variants }: NavbarLogoProps) {
       style={{ transform: "none" }}
     >
       <div className="absolute -inset-3 bg-gradient-to-r from-primary/50 to-primary/10 blur-lg opacity-60 rounded-full pointer-events-none" />
-      <Link href="/" className="relative z-[1] cursor-pointer flex items-center gap-2" prefetch={false}>
+      <Link
+        href="/"
+        className="relative z-[1] cursor-pointer flex items-center gap-2"
+        prefetch={false}
+      >
         <FaUser />
       </Link>
     </motion.div>
