499b1d4552f37987d3e5351bc0415d1bb91f9c2c
Keiran <me@keiran.cc>
2025-05-14 00:13:24 +0100

    perf(layout): pretty sure this is the proper way to do metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,61 +1,72 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { Navbar } from "~/components/Navbar";
 import { NavbarProvider } from "~/context/NavbarContext";
+import "./globals.css";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://slop.sh"),
-  title: "Keiran",
-  description: "I make things",
-  icons: {
-    icon: [
-      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon/favicon.ico", sizes: "any" },
-    ],
-    apple: [
-      {
-        url: "/favicon/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
-    other: [
-      {
-        rel: "android-chrome",
-        url: "/favicon/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        rel: "android-chrome",
-        url: "/favicon/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      { rel: "manifest", url: "/favicon/site.webmanifest" },
-    ],
-  },
-  openGraph: {
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    metadataBase: new URL("https://slop.sh"),
     title: "Keiran",
-    description: "i make things",
-    url: "https://slop.sh",
-    siteName: "slop.sh",
-    images: [
-      {
-        url: "/og-image.png",
-        width: 800,
-        height: 400,
-      },
-    ],
-  },
-};
+    description: "I make things",
+    icons: {
+      icon: [
+        {
+          url: "/favicon/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          url: "/favicon/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        { url: "/favicon/favicon.ico", sizes: "any" },
+      ],
+      apple: [
+        {
+          url: "/favicon/apple-touch-icon.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+      ],
+      other: [
+        {
+          rel: "android-chrome",
+          url: "/favicon/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          rel: "android-chrome",
+          url: "/favicon/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        { rel: "manifest", url: "/favicon/site.webmanifest" },
+      ],
+    },
+    openGraph: {
+      title: "Keiran",
+      description: "i make things",
+      url: "https://slop.sh",
+      siteName: "slop.sh",
+      images: [
+        {
+          url: "/og-image.png",
+          width: 800,
+          height: 400,
+          alt: "brrr",
+        },
+      ],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
