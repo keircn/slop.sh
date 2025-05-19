a9225548bb8994aa76d8a2a6fbdbb8565748ee1a
Keiran <me@keiran.cc>
2025-05-19 16:56:49 +0200

    fix: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cd /home/keiran/prod/slop-new
+
 git fetch origin main
 git reset --hard origin/main
+
 pnpm i
-pnpm build
-pm2 restart 3
-echo DEPLOYMENT SUCCESSFUL
+
+pnpm build && pm2 restart 3
