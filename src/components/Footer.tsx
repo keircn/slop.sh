8b1dbb854afc73ca921e6ef34795d3bc9ff05dc7
Keiran <me@keiran.cc>
2025-05-19 15:05:46 +0100

    feat: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ export function Footer() {
             Keiran
           </a>
         </p>
-        <p className='text-xs'>Last updated: 14th May, 2025</p>
+        <p className='text-xs'>Last updated: 19th May, 2025</p>
       </div>
     </motion.footer>
   );
